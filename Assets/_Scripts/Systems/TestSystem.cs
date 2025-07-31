ee2f5c1f42f5d02941516ef1a543b8276b401bf4
Ludovic Desroches <ldesroches@odux.fr>
2025-07-31 21:24:28 +0200

    Episode 3: Card Architecture
    Signed-off-by: Ludovic Desroches <ldesroches@odux.fr>

--- Assets/_Scripts/Systems/TestSystem.cs
+++ Assets/_Scripts/Systems/TestSystem.cs
@@ -3,11 +3,13 @@ using UnityEngine;
 public class TestSystem : MonoBehaviour
 {
     [SerializeField] private HandView handView;
+    [SerializeField] private CardData cardData;
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            CardView cardView = CardViewCreator.Instance.CreateCardView(transform.position, Quaternion.identity);
+            Card card = new(cardData);
+            CardView cardView = CardViewCreator.Instance.CreateCardView(card, transform.position, Quaternion.identity);
             StartCoroutine(handView.AddCard(cardView));
         }
     }
