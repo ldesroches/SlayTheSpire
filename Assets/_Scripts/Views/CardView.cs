b75642a8567a4b6c0f878f34d297644cbe6fc24c
Ludovic Desroches <ldesroches@odux.fr>
2025-07-31 21:56:56 +0200

    Episode 4: Card Hover System
    Signed-off-by: Ludovic Desroches <ldesroches@odux.fr>

--- Assets/_Scripts/Views/CardView.cs
+++ Assets/_Scripts/Views/CardView.cs
@@ -17,4 +17,15 @@ public class CardView : MonoBehaviour
         mana.text = card.Mana.ToString();
         imageSR.sprite = card.Image;
     }
+    void OnMouseEnter()
+    {
+        wrapper.SetActive(false);
+        Vector3 pos = new(transform.position.x, -2, 0);
+        CardViewHoverSystem.Instance.Show(Card, pos);
+    }
+    void OnMouseExit()
+    {
+        CardViewHoverSystem.Instance.Hide();
+        wrapper.SetActive(true);
+    }
 }
