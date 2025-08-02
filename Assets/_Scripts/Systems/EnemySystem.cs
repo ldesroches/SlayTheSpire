60eb27cf7b11f202cb4bbf17e0926d3f036c2fe0
Ludovic Desroches <ldesroches@odux.fr>
2025-08-02 21:08:46 +0200

    Episode 5: Turn Structure
    Signed-off-by: Ludovic Desroches <ldesroches@odux.fr>

---
+++ Assets/_Scripts/Systems/EnemySystem.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemySystem : MonoBehaviour
+{
+    void OnEnable()
+    {
+        ActionSystem.AttachPerformer<EnemyTurnGA>(EnemyTurnPerformer); 
+    }
+    void OnDisable()
+    {
+        ActionSystem.DetachPerformer<EnemyTurnGA>(); 
+    }
+    private IEnumerator EnemyTurnPerformer(EnemyTurnGA enemyTurnGA)
+    {
+        Debug.Log("Enemy Turn");
+        yield return new WaitForSeconds(2f);
+        Debug.Log("End Enemy Turn");
+    }
+}
