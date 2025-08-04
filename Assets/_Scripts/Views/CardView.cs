f9e3824a667b65b34e6453c5ca4022ecf49a8315
Ludovic Desroches <ldesroches@odux.fr>
2025-08-04 21:40:24 +0200

    Episode 7: Drag Behaviour & Match Setup
    Signed-off-by: Ludovic Desroches <ldesroches@odux.fr>

--- Assets/_Scripts/Views/CardView.cs
+++ Assets/_Scripts/Views/CardView.cs
@@ -9,6 +9,8 @@ public class CardView : MonoBehaviour
     [SerializeField] private SpriteRenderer imageSR;
     [SerializeField] private GameObject wrapper;
     public Card Card { get; private set; }
+    private Vector3 dragStartPosition;
+    private Quaternion dragStartRotation;
     public void Setup(Card card)
     {
         Card = card;
@@ -19,13 +21,47 @@ public class CardView : MonoBehaviour
     }
     void OnMouseEnter()
     {
+        if (!Interactions.Instance.PlayerCanHover()) return;
         wrapper.SetActive(false);
         Vector3 pos = new(transform.position.x, -2, 0);
         CardViewHoverSystem.Instance.Show(Card, pos);
     }
     void OnMouseExit()
     {
+        if (!Interactions.Instance.PlayerCanHover()) return;
         CardViewHoverSystem.Instance.Hide();
         wrapper.SetActive(true);
+    }
+    void OnMouseDown()
+    {
+        if (!Interactions.Instance.PlayerCanInteract()) return;
+        Interactions.Instance.PlayerIsDragging = true;
+        wrapper.SetActive(true);
+        CardViewHoverSystem.Instance.Hide();
+        dragStartPosition = transform.position;
+        dragStartRotation = transform.rotation;
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        transform.position = MouseUtil.GetMousePositionInWorldSpace(-1);
+    }
+    void OnMouseDrag()
+    {
+        if (!Interactions.Instance.PlayerCanInteract()) return;
+        transform.position = MouseUtil.GetMousePositionInWorldSpace(-1);
+
+    }
+    void OnMouseUp()
+    {
+        if (!Interactions.Instance.PlayerCanInteract()) return;
+        if (Physics.Raycast(transform.position, Vector3.forward, out RaycastHit hit, 10f))
+        {
+            // Play card
+        }
+        else
+        {
+            transform.position = dragStartPosition;
+            transform.rotation = dragStartRotation;
+        }
+        Interactions.Instance.PlayerIsDragging = false;
+
     }
 }
