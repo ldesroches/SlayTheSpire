f76e1d88fe728c648738acbd6acf36bfa7e9c1aa
Ludovic Desroches <ldesroches@odux.fr>
2025-08-07 21:46:58 +0200

    Episode 14: Enemy Attack
    Signed-off-by: Ludovic Desroches <ldesroches@odux.fr>

--- Assets/_Scripts/Systems/EnemySystem.cs
+++ Assets/_Scripts/Systems/EnemySystem.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class EnemySystem : Singleton<EnemySystem>
@@ -7,11 +8,13 @@ public class EnemySystem : Singleton<EnemySystem>
     [SerializeField] private EnemyBoardView enemyBoardView;
     void OnEnable()
     {
-        ActionSystem.AttachPerformer<EnemyTurnGA>(EnemyTurnPerformer); 
+        ActionSystem.AttachPerformer<EnemyTurnGA>(EnemyTurnPerformer);
+        ActionSystem.AttachPerformer<AttackHeroGA>(AttackHeroPerformer);
     }
     void OnDisable()
     {
-        ActionSystem.DetachPerformer<EnemyTurnGA>(); 
+        ActionSystem.DetachPerformer<EnemyTurnGA>();
+        ActionSystem.DetachPerformer<AttackHeroGA>();
     }
     public void Setup(List<EnemyData> enemyDatas)
     {
@@ -22,8 +25,20 @@ public class EnemySystem : Singleton<EnemySystem>
     }
     private IEnumerator EnemyTurnPerformer(EnemyTurnGA enemyTurnGA)
     {
-        Debug.Log("Enemy Turn");
-        yield return new WaitForSeconds(2f);
-        Debug.Log("End Enemy Turn");
+        foreach (var enemy in enemyBoardView.EnemyViews)
+        {
+            AttackHeroGA attackHeroGA = new(enemy);
+            ActionSystem.Instance.AddReaction(attackHeroGA);
+        }
+        yield return null;
+    }
+    private IEnumerator AttackHeroPerformer(AttackHeroGA attackHeroGA)
+    {
+        EnemyView attacker = attackHeroGA.Attacker;
+        Tween tween = attacker.transform.DOMoveX(attacker.transform.position.x - 1f, 0.15f);
+        yield return tween.WaitForCompletion();
+        attacker.transform.DOMoveX(attacker.transform.position.x + 1f, 0.25f);
+        DealDamageGA dealDamageGA = new(attacker.AttackPower, new() { HeroSystem.Instance.HeroView });
+        ActionSystem.Instance.AddReaction(dealDamageGA);
     }
 }
