b4d72707293edb4a91f0c23b69a7ecb0ad601227
Ludovic Desroches <ldesroches@odux.fr>
2025-08-07 22:48:46 +0200

    Episode 16: Automatic Targeting System
    Signed-off-by: Ludovic Desroches <ldesroches@odux.fr>

--- Assets/_Scripts/Effects/DealDamageEffect.cs
+++ Assets/_Scripts/Effects/DealDamageEffect.cs
@@ -4,9 +4,8 @@ using UnityEngine;
 public class DealDamageEffect : Effect
 {
     [SerializeField] private int damageAmount;
-    public override GameAction GetGameAction()
+    public override GameAction GetGameAction(List<CombatantView> targets)
     {
-        List<CombatantView> targets = new(EnemySystem.Instance.Enemies);
         DealDamageGA dealDamageGA = new(damageAmount, targets);
         return dealDamageGA;
     }
