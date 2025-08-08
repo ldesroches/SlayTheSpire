cc29a56eb3683943143d5d2a4fa256cd2a68bcbd
Ludovic Desroches <ldesroches@odux.fr>
2025-08-08 18:38:39 +0200

    Episode 18: Manual Targeting System
    Signed-off-by: Ludovic Desroches <ldesroches@odux.fr>

--- Assets/_Scripts/GameActions/PlayCardGA.cs
+++ Assets/_Scripts/GameActions/PlayCardGA.cs
@@ -2,9 +2,16 @@ using UnityEngine;
 
 public class PlayCardGA : GameAction
 {
+    public EnemyView ManualTarget { get; private set; }
     public Card Card { get; set; }
     public PlayCardGA(Card card)
     {
         Card = card;
+        ManualTarget = null;
+    }
+    public PlayCardGA(Card card, EnemyView target)
+    {
+        Card = card;
+        ManualTarget = target;
     }
 }
